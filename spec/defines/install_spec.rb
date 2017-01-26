16da86209154f6e23bc82c5832c7324a6d2b52e6
abloomston <adam.bloomston@cloverhealth.com>
2017-01-26 08:46:52 -0800

    attempt to fix test

--- spec/defines/install_spec.rb
+++ spec/defines/install_spec.rb
@@ -11,10 +11,10 @@ describe 'pyenv::install', :type => :define do
         with_command("git clone https://github.com/yyuu/pyenv.git /home/#{user}/.pyenv")
     end
 
-    it "appends in a rc file, a command to include .pyenv/bin folder in PATH env variable" do
-      should contain_exec("pyenv::shrc #{user}").
-        with_command("echo 'source /home/#{user}/.pyenvrc' >> /home/#{user}/.profile").
-        with_path(['/bin','/usr/bin','/usr/sbin'])
+    it "adds a line to the rc file, a command to include .pyenv/bin folder in PATH env variable" do
+      should contain_file_line("pyenv::shrc #{user}").
+        with_line("source /home/#{user}/.pyenvrc").
+        with_path("/home/#{user}/.profile")
     end
   end
 end
